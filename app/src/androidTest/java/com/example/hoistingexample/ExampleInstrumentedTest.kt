ad92e2022096c35e16649f4b5c126f9f5da5483f
andersbor <anbo@zealand.dk>
2024-09-16 20:01:17 +0200

    rememberSaveable

--- app/src/androidTest/java/com/example/hoistingexample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/hoistingexample/ExampleInstrumentedTest.kt
@@ -1,5 +1,8 @@
 package com.example.hoistingexample
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +10,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +24,8 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.hoistingexample", appContext.packageName)
+
+
     }
+
 }
--- app/src/main/java/com/example/hoistingexample/MainActivity.kt
+++ app/src/main/java/com/example/hoistingexample/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     Column(modifier = Modifier.padding(innerPadding)) {
                         Counter()
 
-                        var count by remember { mutableIntStateOf(0) }
+                        var count by rememberSaveable { mutableIntStateOf(0) }
                         CounterHoistedState(
                             count = count,
                             onCountChange = { count = it },
