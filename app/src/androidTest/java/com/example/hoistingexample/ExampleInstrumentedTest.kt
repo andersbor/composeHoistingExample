52841e2fc7990b059f7ff085beb596483f70641b
andersbor <anbo@zealand.dk>
2025-02-16 12:14:55 +0100

    gradle update

--- app/src/androidTest/java/com/example/hoistingexample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/hoistingexample/ExampleInstrumentedTest.kt
@@ -1,16 +1,10 @@
 package com.example.hoistingexample
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/main/java/com/example/hoistingexample/MainActivity.kt
+++ app/src/main/java/com/example/hoistingexample/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hoistingexample.ui.theme.HoistingExampleTheme
@@ -28,16 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HoistingExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Counter()
-
-                        var count by rememberSaveable { mutableIntStateOf(0) }
-                        CounterHoistedState(
-                            count = count,
-                            onCountChange = { count = it },
-                        )
-                        Text(text = "Count: $count")
-                    }
+                    MainContent(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -45,10 +40,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Counter(modifier: Modifier = Modifier) {
+private fun MainContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        CounterInternalState()
+
+        var count by rememberSaveable { mutableIntStateOf(0) }
+        CounterHoistedState(
+            count = count,
+            onCountChange = { count = it },
+        )
+        Text(text = "Count: $count")
+    }
+}
+
+@Composable
+fun CounterInternalState(modifier: Modifier = Modifier) {
     var count by remember { mutableIntStateOf(0) }
 
-    Column(modifier = modifier.padding(16.dp)) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .border(
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(4.dp)
+            ).padding(8.dp)
+    ) {
         Text("You've clicked $count times")
         Button(onClick = { count++ }) {
             Text("Click me")
@@ -73,7 +89,7 @@ fun CounterHoistedState(
 @Composable
 fun GreetingPreview() {
     HoistingExampleTheme {
-        Counter()
+        CounterInternalState()
     }
 }
 
